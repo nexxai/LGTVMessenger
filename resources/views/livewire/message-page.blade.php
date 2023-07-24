c6da96bbdc4ec35f53cf3420ba71f017c4b3179f
JT Smith <nexxai@gmail.com>
2023-07-24 01:22:35 -0600

    Shrinking max width a little to fit an iPhone 12 Pro Max

--- resources/views/livewire/message-page.blade.php
+++ resources/views/livewire/message-page.blade.php
@@ -1,31 +1,30 @@
-<div class="max-w-4xl px-4 py-2 mx-auto mt-16 lg:px-2 mb-4 font-space-grotesk">
-    <div class="text-6xl font-semibold mb-16 text-slate-200">
+<div class="max-w-2xl px-4 py-2 mx-auto mt-16 mb-4 lg:px-2 font-space-grotesk">
+    <div class="mb-16 text-6xl font-semibold text-slate-200">
         TV Messenger
     </div>
     <form wire:submit.prevent="sendMessage">
-        <div class="space-y-4 mb-12">
-            <span class="text-xl block text-slate-400">Choose device</span>
+        <div class="mb-12 space-y-4">
+            <span class="block text-xl text-slate-400">Choose device</span>
             <select wire:model="selectedTV"
-                class="border w-full text-3xl rounded bg-slate-200 mb-4 px-2 py-2 hover:ring-2 hover:ring-green-600 hover:ring-offset-4 hover:ring-offset-slate-800 focus:outline-0">
+                class="w-full px-2 py-2 mb-4 text-3xl border rounded bg-slate-200 hover:ring-2 hover:ring-green-600 hover:ring-offset-4 hover:ring-offset-slate-800 focus:outline-0">
                 @foreach ($tvList as $key => $val)
                 <option value="{{ $key }}">{{ $val['name'] }}</option>
                 @endforeach
             </select>
         </div>
 
-        <div class="space-y-4 mb-20">
-            <span class="text-xl block text-slate-400">Enter text to send to TV</span>
+        <div class="mb-20 space-y-4">
+            <span class="block text-xl text-slate-400">Enter text to send to TV</span>
             <input wire:model.defer="messageToSend" name="messageToSend"
-                class="border w-full text-3xl rounded bg-slate-200 px-3 py-2 hover:ring-2 hover:ring-green-600 hover:ring-offset-4 hover:ring-offset-slate-800
-               active:ring-2 active:ring-green-600 active:ring-offset-4 active:ring-offset-slate-800 focus:ring-2 focus:ring-green-600 focus:ring-offset-4 focus:ring-offset-slate-800 focus:outline-0"
+                class="w-full px-3 py-2 text-3xl border rounded bg-slate-200 hover:ring-2 hover:ring-green-600 hover:ring-offset-4 hover:ring-offset-slate-800 active:ring-2 active:ring-green-600 active:ring-offset-4 active:ring-offset-slate-800 focus:ring-2 focus:ring-green-600 focus:ring-offset-4 focus:ring-offset-slate-800 focus:outline-0"
                 placeholder="e.g. Lili is awake" />
             @error('messageToSend') <div x-data="{ show: true }" x-show="show"
-                x-init="setTimeout(() => show = false, 3000)" class="text-lg block text-red-400 alert">{{ $message }}
+                x-init="setTimeout(() => show = false, 3000)" class="block text-lg text-red-400 alert">{{ $message }}
             </div>@enderror
         </div>
 
-        <div class="space-y-4 mb-20">
-            <span class="text-xl block text-slate-400">How many times to send</span>
+        <div class="mb-20 space-y-4">
+            <span class="block text-xl text-slate-400">How many times to send</span>
             <div class="flex justify-between">
                 <button
                     class="px-8 py-8 rounded border border-slate-200 transition text-slate-200 {{ $sendNumOfTimes == 1 ? 'bg-slate-400' : 'bg-slate-500' }}"
@@ -46,10 +45,10 @@
         </div>
 
         <button type="submit"
-            class="block rounded text-slate-300 border border-slate-400 px-8 py-4 w-full font-bold transition bg-slate-700 hover:bg-slate-600  hover:border-slate-300 active:bg-slate-600  active:border-slate-300 text-4xl">
+            class="block w-full px-8 py-4 text-4xl font-bold transition border rounded text-slate-300 border-slate-400 bg-slate-700 hover:bg-slate-600 hover:border-slate-300 active:bg-slate-600 active:border-slate-300">
             Send message to TV
         </button>
-        @error('messageToSend') <span class="text-lg block text-red-400 alert">{{ $errorWhenSending }}</span>@enderror
+        @error('messageToSend') <span class="block text-lg text-red-400 alert">{{ $errorWhenSending }}</span>@enderror
 
     </form>
 </div>
